cb40077be94f0a4ede5d5c00d23fe67bb1da65aa
sjwatts119 <samuel.watts67@gmail.com>
2022-04-07 00:54:27 +0100

    Adding brackets to sent from email address

--- php/create_enrolment.php
+++ php/create_enrolment.php
@@ -43,7 +43,7 @@ else{
             if($stmt->execute()){
                 $result = $stmt->get_result();
                 $user = $result->fetch_assoc();
-                $to = $user["email"];
+                $to = "<" . $user["email"] . ">";
                 $subject = "You have been Enrolled on a Course";
                 $message = "<h3>Hello " . $user["email"] . ",</br></br>You have been successfully enrolled on course: " . $courses["courseTitle"] . ",</br></br>If you have any questions regarding this, please contact an admin.";
                 $headers = 'From: <webmaster@WS296281-wad.remote.ac>' . "\r\n" .
