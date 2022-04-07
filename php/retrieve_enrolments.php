f1ceb58867cfdd62fc73cab9ad83e25b1c7930ee
sjwatts119 <samuel.watts67@gmail.com>
2022-04-07 07:35:35 +0100

    Fixing blank flickity instance when the user is enrolled on all courses

--- php/retrieve_enrolments.php
+++ php/retrieve_enrolments.php
@@ -14,6 +14,7 @@ $coursesForTotal = mysqli_query($db_connect, $sql);
 $sql = "SELECT enrolmentID, userID, courseID FROM enrolments WHERE userID =" . $_SESSION['userID'];
 $enrolments = mysqli_query($db_connect, $sql);
 
+//if no enrolments found for current user, return a card stating they can enrol on one of the available courses below.
 if (mysqli_num_rows($enrolments) == 0){
     echo "<div id='card' class='card'>";
     echo "<div id='card-body' class='card-body'>";
@@ -22,49 +23,49 @@ if (mysqli_num_rows($enrolments) == 0){
     echo "</div>";
     echo "</div>";
 }
+else{
+    while ($row = mysqli_fetch_assoc($enrolments)) {
 
-while ($row = mysqli_fetch_assoc($enrolments)) {
-
-    echo "<div id='card' class='card'>";
-    echo "<div id='card-body' class='card-body'>";
-
-    $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses WHERE courseID=" . $row["courseID"];
-    $courses = mysqli_query($db_connect, $sql);
-    $courses = $courses->fetch_assoc();
-
-    //Iterating through each column on the table and outputting it into a card
-    for ($iteration = 1; $iteration < 6; $iteration++) {
-
-        if($iteration == 1){
-            //Inputs the current field into the card
-            echo "<h5 class='card-title'>" . $courses["courseTitle"] . "</h5>"; 
+        echo "<div id='card' class='card'>";
+        echo "<div id='card-body' class='card-body'>";
+    
+        $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses WHERE courseID=" . $row["courseID"];
+        $courses = mysqli_query($db_connect, $sql);
+        $courses = $courses->fetch_assoc();
+    
+        //Iterating through each column on the table and outputting it into a card
+        for ($iteration = 1; $iteration < 6; $iteration++) {
+    
+            if($iteration == 1){
+                //Inputs the current field into the card
+                echo "<h5 class='card-title'>" . $courses["courseTitle"] . "</h5>"; 
+            }
+            else if($iteration == 2){
+                //Inputs the current field into the card
+                echo "<p class='card-text'>Course Date: " . $courses["courseDate"] . "</p>";
+            }
+            else if($iteration == 3){
+                //Inputs the current field into the card
+                echo "<p class='card-text'>Course Duration (Weeks): " . $courses["courseDuration"] . "</p>";
+            }
+            else if($iteration == 4){
+                //find number of enrolments for current course to figure out if the course is at capacity or not.
+                $sql = "SELECT enrolmentID FROM enrolments WHERE courseID=" . $courses["courseID"];
+                $enrolmentTotal = mysqli_query($db_connect, $sql);
+                $enrolmentTotal = mysqli_num_rows($enrolmentTotal);
+    
+                //Inputs the current field into the card
+                echo "<p class='card-text'>Current Attendees: " . $enrolmentTotal . "/" . $courses["maxAttendees"] . "</p>";
+            }
+            else if($iteration == 5){
+                //Inputs the current field into the card
+                echo "<p class='card-text card-description'>Course Description: " . $courses["courseDescription"] . "</p>";
+            }
         }
-        else if($iteration == 2){
-            //Inputs the current field into the card
-            echo "<p class='card-text'>Course Date: " . $courses["courseDate"] . "</p>";
-        }
-        else if($iteration == 3){
-            //Inputs the current field into the card
-            echo "<p class='card-text'>Course Duration (Weeks): " . $courses["courseDuration"] . "</p>";
-        }
-        else if($iteration == 4){
-            //find number of enrolments for current course to figure out if the course is at capacity or not.
-            $sql = "SELECT enrolmentID FROM enrolments WHERE courseID=" . $courses["courseID"];
-            $enrolmentTotal = mysqli_query($db_connect, $sql);
-            $enrolmentTotal = mysqli_num_rows($enrolmentTotal);
-
-            //Inputs the current field into the card
-            echo "<p class='card-text'>Current Attendees: " . $enrolmentTotal . "/" . $courses["maxAttendees"] . "</p>";
-        }
-        else if($iteration == 5){
-            //Inputs the current field into the card
-            echo "<p class='card-text card-description'>Course Description: " . $courses["courseDescription"] . "</p>";
-        }
+        //adds button to end of table with ID the same as the current UID of the row for the course
+        echo "<a href='#' class='cancelEnrolmentButton btn btn-danger' id='" . $row['enrolmentID'] . "'>Cancel</a>";
+        echo "</div>";
+        echo "</div>";
     }
-    //adds button to end of table with ID the same as the current UID of the row for the course
-    echo "<a href='#' class='cancelEnrolmentButton btn btn-danger' id='" . $row['enrolmentID'] . "'>Cancel</a>";
-    echo "</div>";
-    echo "</div>";
 }
-
 ?>
--- php/retrieve_non_enrolled.php
+++ php/retrieve_non_enrolled.php
@@ -17,26 +17,87 @@ $arrayOfEnrolledCourses = array_unique($arrayOfEnrolledCourses);
 $implodedArrayOfEnrolledCourses = implode( ',', $arrayOfEnrolledCourses);
 
 $sql = "SELECT enrolmentID, userID, courseID FROM enrolments WHERE userID =" . $_SESSION['userID'];
-$enrolments = mysqli_query($db_connect, $sql); 
+$enrolments = mysqli_query($db_connect, $sql);
 
 $nonEnrolledCourses = "SELECT courseID, courseTitle, courseDate, maxAttendees FROM courses WHERE courseID NOT IN (" . $implodedArrayOfEnrolledCourses . ")";
-$nonEnrolled = mysqli_query($db_connect, $nonEnrolledCourses); 
+$nonEnrolled = mysqli_query($db_connect, $nonEnrolledCourses);
 
-//If user not enrolled on any courses, display all courses in unenrolled section.
-if (!$nonEnrolled){
+$sql = "SELECT courseID FROM courses";
+$totalCourses = mysqli_query($db_connect, $sql);
 
-    $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses";
-    $result = mysqli_query($db_connect, $sql); 
+//if use is enrolled on all courses, return a card stating there are no more courses to enrol on.
+if (mysqli_num_rows($enrolments) >= mysqli_num_rows($totalCourses)){
+    echo "<div id='card' class='card'>";
+    echo "<div id='card-body' class='card-body'>";
+    echo "<h5 class='card-title'>There are currently no more courses to enrol on.</h5>";
+    echo "<p class='card-text'>If you're waiting for a course to become available, please check back later or contact a system Admin.</p>";
+    echo "</div>";
+    echo "</div>";
+}
+else{
+    //If user not enrolled on any courses, display all courses in unenrolled section.
+    if (!$nonEnrolled){
 
-    while ($row = mysqli_fetch_assoc($result)) {
+        $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses";
+        $result = mysqli_query($db_connect, $sql); 
 
-        echo "<div id='card' class='card'>";
-        echo "<div id='card-body' class='card-body'>";
-    
-        $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses WHERE courseID=" . $row["courseID"];
-        $courses = mysqli_query($db_connect, $sql);
-        $courses = $courses->fetch_assoc();
-    
+        while ($row = mysqli_fetch_assoc($result)) {
+
+            echo "<div id='card' class='card'>";
+            echo "<div id='card-body' class='card-body'>";
+        
+            $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses WHERE courseID=" . $row["courseID"];
+            $courses = mysqli_query($db_connect, $sql);
+            $courses = $courses->fetch_assoc();
+        
+            for ($iteration = 1; $iteration < 6; $iteration++) {
+
+                if($iteration == 1){
+                    //Inputs the current field into the card
+                    echo "<h5 class='card-title'>" . $courses["courseTitle"] . "</h5>"; 
+                }
+                else if($iteration == 2){
+                    //Inputs the current field into the card
+                    echo "<p class='card-text'>Course Date: " . $courses["courseDate"] . "</p>";
+                }
+                else if($iteration == 3){
+                    //Inputs the current field into the card
+                    echo "<p class='card-text'>Course Duration (Weeks): " . $courses["courseDuration"] . "</p>";
+                }
+                else if($iteration == 4){
+                    //find number of enrolments for current course to figure out if the course is at capacity or not.
+                    $sql = "SELECT enrolmentID FROM enrolments WHERE courseID=" . $courses["courseID"];
+                    $enrolmentTotal = mysqli_query($db_connect, $sql);
+                    $enrolmentTotal = mysqli_num_rows($enrolmentTotal);
+        
+                    //Inputs the current field into the card
+                    echo "<p class='card-text'>Current Attendees: " . $enrolmentTotal . "/" . $courses["maxAttendees"] . "</p>";
+                }
+                else if($iteration == 5){
+                    //Inputs the current field into the card
+                    echo "<p class='card-text card-description'>Course Description: " . $courses["courseDescription"] . "</p>";
+                }
+            }
+            
+            //adds button to end of table with ID the same as the current UID of the row for the course
+            echo "<a href='#' class='enrolButton btn btn-primary' id='" . $row["courseID"] . "'>Enrol</a>";
+            echo "</div>";
+            echo "</div>";
+
+        }
+    }
+
+    //If user is enrolled on courses, display only the courses they aren't enrolled on in unenrolled section.
+    else{
+        while ($row = mysqli_fetch_assoc($nonEnrolled)) {
+
+            echo "<div id='card' class='card'>";
+            echo "<div id='card-body' class='card-body'>";
+        
+            $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses WHERE courseID=" . $row["courseID"];
+            $courses = mysqli_query($db_connect, $sql);
+            $courses = $courses->fetch_assoc();
+        
         for ($iteration = 1; $iteration < 6; $iteration++) {
 
             if($iteration == 1){
@@ -56,7 +117,7 @@ if (!$nonEnrolled){
                 $sql = "SELECT enrolmentID FROM enrolments WHERE courseID=" . $courses["courseID"];
                 $enrolmentTotal = mysqli_query($db_connect, $sql);
                 $enrolmentTotal = mysqli_num_rows($enrolmentTotal);
-    
+
                 //Inputs the current field into the card
                 echo "<p class='card-text'>Current Attendees: " . $enrolmentTotal . "/" . $courses["maxAttendees"] . "</p>";
             }
@@ -65,60 +126,13 @@ if (!$nonEnrolled){
                 echo "<p class='card-text card-description'>Course Description: " . $courses["courseDescription"] . "</p>";
             }
         }
-        
+            
         //adds button to end of table with ID the same as the current UID of the row for the course
         echo "<a href='#' class='enrolButton btn btn-primary' id='" . $row["courseID"] . "'>Enrol</a>";
         echo "</div>";
         echo "</div>";
-
-    }
-}
-
-//If user is enrolled on courses, display only the courses they aren't enrolled on in unenrolled section.
-else{
-    while ($row = mysqli_fetch_assoc($nonEnrolled)) {
-
-        echo "<div id='card' class='card'>";
-        echo "<div id='card-body' class='card-body'>";
-    
-        $sql = "SELECT courseID, courseTitle, courseDate, courseDuration, maxAttendees, courseDescription FROM courses WHERE courseID=" . $row["courseID"];
-        $courses = mysqli_query($db_connect, $sql);
-        $courses = $courses->fetch_assoc();
-    
-    for ($iteration = 1; $iteration < 6; $iteration++) {
-
-        if($iteration == 1){
-            //Inputs the current field into the card
-            echo "<h5 class='card-title'>" . $courses["courseTitle"] . "</h5>"; 
-        }
-        else if($iteration == 2){
-            //Inputs the current field into the card
-            echo "<p class='card-text'>Course Date: " . $courses["courseDate"] . "</p>";
-        }
-        else if($iteration == 3){
-            //Inputs the current field into the card
-            echo "<p class='card-text'>Course Duration (Weeks): " . $courses["courseDuration"] . "</p>";
-        }
-        else if($iteration == 4){
-            //find number of enrolments for current course to figure out if the course is at capacity or not.
-            $sql = "SELECT enrolmentID FROM enrolments WHERE courseID=" . $courses["courseID"];
-            $enrolmentTotal = mysqli_query($db_connect, $sql);
-            $enrolmentTotal = mysqli_num_rows($enrolmentTotal);
-
-            //Inputs the current field into the card
-            echo "<p class='card-text'>Current Attendees: " . $enrolmentTotal . "/" . $courses["maxAttendees"] . "</p>";
-        }
-        else if($iteration == 5){
-            //Inputs the current field into the card
-            echo "<p class='card-text card-description'>Course Description: " . $courses["courseDescription"] . "</p>";
-        }
-    }
         
-    //adds button to end of table with ID the same as the current UID of the row for the course
-    echo "<a href='#' class='enrolButton btn btn-primary' id='" . $row["courseID"] . "'>Enrol</a>";
-    echo "</div>";
-    echo "</div>";
-    
+        }
     }
 }
 
