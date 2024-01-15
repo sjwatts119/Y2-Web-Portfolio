44c9183792ace68c628613fa81150bb7ccd31bed
sjwatts119 <samuel.watts67@gmail.com>
2024-01-15 15:40:38 +0000

    .env File for Database Connection

--- php/_connect.php
+++ php/_connect.php
@@ -1,9 +1,13 @@
 <?php
 
-$db_server = "plesk.remote.ac";
-$db_user = "WS296281_WAD";
-$db_password = "cr8m2E1^";
-$db_database = "WS296281_WAD";
+require_once($_SERVER['DOCUMENT_ROOT'].'/php/read_env_vars.php');
+
+GetSettings();
+
+$db_server = $_ENV['DB_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_password = $_ENV['DB_PASS'];
+$db_database = $_ENV['DB_NAME'];
 
 //connect to database server
 $db_connect = mysqli_connect($db_server,$db_user,$db_password,$db_database)
